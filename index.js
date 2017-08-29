5810ac84db88c6cc2716736ded0554f71305b4b6
Subhajit Sahu <wolfram77@gmail.com>
2017-08-29 08:30:56 +0530

    :u5408: log process.env on http req

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log(process.env);
+var http = require('http');
+
+http.createServer(function(req, res) {
+  console.log(process.env);
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('Hello World!');
+  res.end();
+}).listen(process.env.PORT);
